6b7415abadff11d195b1831ae76b56acddb06399
weepingdog <farewellwhu@gmail.com>
2016-02-06 18:57:11 +0800

    project start

---
+++ VerticesToCenter/ButtonOptionVerticesToCenter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.ArcMapUI;
+namespace VerticesToCenter
+{
+    public class ButtonOptionVerticesToCenter : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public ButtonOptionVerticesToCenter()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            //
+            //  TODO: Sample code showing how to access button host
+            //
+            
+        }
+        protected override void OnUpdate()
+        {
+            //Enabled = ArcMap.Application != null;
+            Enabled = (ArcMap.Application != null);
+        }
+    }
+
+}
---
+++ VerticesToCenter/ToolVerticesToCenter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.Display;
+using ESRI.ArcGIS.Geodatabase;
+
+namespace VerticesToCenter
+{
+    public class ToolVerticesToCenter : ESRI.ArcGIS.Desktop.AddIns.Tool
+    {        
+        #region "1 System Event"
+        public ToolVerticesToCenter()
+        {
+        }
+
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;         
+        }
+
+        protected override void OnActivate()
+        {
+           
+        }
+
+        protected override bool OnDeactivate()
+        {
+            return base.OnDeactivate();
+        }
+        #endregion
+
+        #region "2 Mouse Event"
+        protected override void OnMouseDown(ESRI.ArcGIS.Desktop.AddIns.Tool.MouseEventArgs arg)
+        {
+            
+        }
+
+        protected override void OnMouseMove(ESRI.ArcGIS.Desktop.AddIns.Tool.MouseEventArgs arg)
+        {
+            
+        }
+
+        protected override void OnMouseUp(ESRI.ArcGIS.Desktop.AddIns.Tool.MouseEventArgs arg)
+        {
+          
+        }
+        #endregion
+    }
+
+}
