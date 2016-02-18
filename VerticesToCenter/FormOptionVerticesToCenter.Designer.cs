c13ac1299812fe5b540217a5dda3ef4045e1d1e6
weepingdog <farewellwhu@gmail.com>
2016-02-18 10:58:42 +0800

    add pixelOption and MaxFeatureSelect

--- VerticesToCenter/FormOptionVerticesToCenter.Designer.cs
+++ VerticesToCenter/FormOptionVerticesToCenter.Designer.cs
@@ -37,9 +37,20 @@
             this.checkBox_SnapToCenter = new System.Windows.Forms.CheckBox();
             this.comboBox_SelectMode = new System.Windows.Forms.ComboBox();
             this.label_SelectMode = new System.Windows.Forms.Label();
+            this.groupBox_Pixel = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
             this.tabControl.SuspendLayout();
             this.tabPage_SelectLayers.SuspendLayout();
             this.tabPage_Setting.SuspendLayout();
+            this.groupBox_Pixel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
             this.SuspendLayout();
             // 
             // checkedListBox_EditablePolyLineNames
@@ -95,6 +106,9 @@
             // 
             // tabPage_Setting
             // 
+            this.tabPage_Setting.Controls.Add(this.numericUpDown1);
+            this.tabPage_Setting.Controls.Add(this.label3);
+            this.tabPage_Setting.Controls.Add(this.groupBox_Pixel);
             this.tabPage_Setting.Controls.Add(this.label_SelectMode);
             this.tabPage_Setting.Controls.Add(this.comboBox_SelectMode);
             this.tabPage_Setting.Controls.Add(this.checkBox_SnapToCenter);
@@ -133,6 +147,102 @@
             this.label_SelectMode.TabIndex = 2;
             this.label_SelectMode.Text = "SelectMode";
             // 
+            // groupBox_Pixel
+            // 
+            this.groupBox_Pixel.Controls.Add(this.numericUpDown3);
+            this.groupBox_Pixel.Controls.Add(this.numericUpDown2);
+            this.groupBox_Pixel.Controls.Add(this.label2);
+            this.groupBox_Pixel.Controls.Add(this.label1);
+            this.groupBox_Pixel.Location = new System.Drawing.Point(12, 64);
+            this.groupBox_Pixel.Name = "groupBox_Pixel";
+            this.groupBox_Pixel.Size = new System.Drawing.Size(193, 91);
+            this.groupBox_Pixel.TabIndex = 3;
+            this.groupBox_Pixel.TabStop = false;
+            this.groupBox_Pixel.Text = "PixelOption";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RadiusChangeLimit";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "MaxRadius";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "MaxFeatureSelect";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(146, 168);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(51, 21);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(134, 25);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(55, 21);
+            this.numericUpDown2.TabIndex = 2;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(134, 53);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(53, 21);
+            this.numericUpDown3.TabIndex = 3;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            // 
             // FormOptionVerticesToCenter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -145,6 +255,11 @@
             this.tabPage_SelectLayers.ResumeLayout(false);
             this.tabPage_Setting.ResumeLayout(false);
             this.tabPage_Setting.PerformLayout();
+            this.groupBox_Pixel.ResumeLayout(false);
+            this.groupBox_Pixel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -160,5 +275,12 @@
         private System.Windows.Forms.CheckBox checkBox_SnapToCenter;
         private System.Windows.Forms.Label label_SelectMode;
         private System.Windows.Forms.ComboBox comboBox_SelectMode;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox_Pixel;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
