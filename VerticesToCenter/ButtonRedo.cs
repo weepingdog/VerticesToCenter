93d82c4d57c8901332ec2e3ac9c3cc7619bf2b56
weepingdog <farewellwhu@gmail.com>
2016-02-22 20:16:22 +0800

    add Redo and Undo Button

---
+++ VerticesToCenter/ButtonRedo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace VerticesToCenter
+{
+    public class ButtonRedo : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public ButtonRedo()
+        {
+        }
+
+        protected override void OnClick()
+        {
+        }
+
+        protected override void OnUpdate()
+        {
+            Enabled = (ArcMap.Application != null && GlobeStatus.IsEditing);
+        }
+    }
+}
---
+++ VerticesToCenter/ButtonUndo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace VerticesToCenter
+{
+    public class ButtonUndo : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public ButtonUndo()
+        {
+        }
+
+        protected override void OnClick()
+        {
+        }
+
+        protected override void OnUpdate()
+        {
+
+            Enabled = (ArcMap.Application != null && GlobeStatus.IsEditing);
+        }
+    }
+}
