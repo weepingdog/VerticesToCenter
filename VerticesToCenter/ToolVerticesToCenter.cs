ab4404c184bbdbd12c5c7e95736a5a419fd99ec1
weepingdog <farewellwhu@gmail.com>
2016-02-22 09:50:56 +0800

    Tool MouseMoveUp function finish

--- VerticesToCenter/ToolVerticesToCenter.cs
+++ VerticesToCenter/ToolVerticesToCenter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Forms;
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
@@ -15,13 +16,13 @@ namespace VerticesToCenter
     {
         private bool m_isMouseDown = false;
         private IPoint m_PointCentre;
+        private IPoint m_PointMouseMoveTo;
         
         private INewCircleFeedback m_CircleFeedback;    //3 跟踪圆
 
         //private int m_LastSelectFeatureCount = 0;
         private double m_LastRadius = 0;        
         private IList<TrackPoint> m_TrackPointList = new List<TrackPoint>();
-        int m_LastTrackPointCount = 0;
 
         #region "1 System Event"
         public ToolVerticesToCenter()
@@ -53,11 +54,11 @@ namespace VerticesToCenter
             GlobeStatus.ActiveView.PartialRefresh(esriViewDrawPhase.esriViewGeoSelection, null, null);
             //GlobeStatus.ActiveView.Refresh(); 
 
-            //鼠标按下点作为圆心点//或者捕捉
             IPoint PointMouseDown = GlobeStatus.ActiveView.ScreenDisplay.DisplayTransformation.ToMapPoint(arg.X, arg.Y);
             if (PointMouseDown == null)
                 return;
-            if (GlobeStatus.CenterSnap)
+
+            if (GlobeStatus.CenterSnap) //若开启了圆心捕捉
             {
                 m_PointCentre = PointSnapWhenMouseDown(PointMouseDown, GlobeStatus.PixelRadiusChangeLimit);
             }
@@ -68,9 +69,8 @@ namespace VerticesToCenter
 
             //开始追踪圆
             CircleFeedBackWhenMouseDown(m_PointCentre);
-            
-            //开始追踪移动点
-            TrackPointFeedBackWhenMouseDown(m_PointCentre);
+
+            FeatureSelectWhenMouseDown(m_PointCentre);
 
             m_isMouseDown = true;
         }
@@ -84,39 +84,33 @@ namespace VerticesToCenter
                 m_CircleFeedback.Start(pointCentre);
             }
         }
-        
-        private void TrackPointFeedBackWhenMouseDown(IPoint pointCentre)
+        private void FeatureSelectWhenMouseDown(IPoint pointCentre)
         {
-            //正常情况下，此时TrackPointCount为0
-            //int TrackPointCount = m_TrackPointList.Count;
-            //for (int i = TrackPointCount-1; i >0 ; i--)
-            //    m_TrackPointList[i].NewLineFeedback = null;
-            //m_TrackPointList.Clear();
+            m_TrackPointList.Clear();
         }
 
         protected override void OnMouseMove(ESRI.ArcGIS.Desktop.AddIns.Tool.MouseEventArgs arg)
         {
-            //鼠标未按下无效（要等同于拖曳操作）,但提示捕捉圆心点(在开启捕捉情况下)
-            if (!m_isMouseDown)
+            //移动到的点
+            m_PointMouseMoveTo = GlobeStatus.ActiveView.ScreenDisplay.DisplayTransformation.ToMapPoint(arg.X, arg.Y);
+            if (m_PointMouseMoveTo == null)
+                return;
+
+            if (!m_isMouseDown) //鼠标未按下无仅提示捕捉圆心点(在开启捕捉情况下)
             {
                 if (GlobeStatus.CenterSnap)
                     PointSnapWhenMouseMove();
                 return; 
             }
-
-            //移动到的点
-            IPoint PointMouseMoveTo = GlobeStatus.ActiveView.ScreenDisplay.DisplayTransformation.ToMapPoint(arg.X, arg.Y);
-            if (PointMouseMoveTo == null)
-                return;
-
+            
             GlobeStatus.ActiveView.PartialRefresh(esriViewDrawPhase.esriViewGeoSelection, null, null);
             GlobeStatus.ActiveView.PartialRefresh(esriViewDrawPhase.esriViewGraphics, null, null);
             
             //跟踪圆
-            CircleFeedBackWhenMouseMove(PointMouseMoveTo);
+            CircleFeedBackWhenMouseMove(m_PointMouseMoveTo);
            
             //追踪多个待移动的点
-            TrackPointFeedBackWhenMouseMove(m_PointCentre,PointMouseMoveTo,GlobeStatus.SelectMode);
+            FeatureSelectWhenMouseMove(m_PointCentre,m_PointMouseMoveTo,GlobeStatus.SelectMode);
 
             GlobeStatus.ActiveView.PartialRefresh(esriViewDrawPhase.esriViewGeoSelection, null, null);
             GlobeStatus.ActiveView.PartialRefresh(esriViewDrawPhase.esriViewGraphics, null, null);
@@ -134,9 +128,8 @@ namespace VerticesToCenter
             }
             if (m_CircleFeedback != null)
                 m_CircleFeedback.MoveTo(pointMouseMoveTo);
-        }
-     
-        private void TrackPointFeedBackWhenMouseMove(IPoint pointCentre, IPoint pointMouseMoveTo, EnumSelectMode SelectMode)
+        }     
+        private void FeatureSelectWhenMouseMove(IPoint pointCentre, IPoint pointMouseMoveTo, EnumSelectMode SelectMode)
         {
             double currentRadius = FunctionCommon.GetDistance2P(m_PointCentre, pointMouseMoveTo);
 
@@ -157,72 +150,7 @@ namespace VerticesToCenter
             IGeometry pGeometry = FunctionCommon.GetCircleGeometry(m_PointCentre, currentRadius);
             ISelectionEnvironment env = new SelectionEnvironmentClass();
             env.CombinationMethod = esriSelectionResultEnum.esriSelectionResultNew;
-            GlobeStatus.Map.SelectByShape(pGeometry, env, false);
-            GlobeStatus.ActiveView.PartialRefresh(esriViewDrawPhase.esriViewGeoSelection, null, pGeometry.Envelope);
-            int currentSelectFeatureCount=GlobeStatus.Map.SelectionCount;
-
-            //4 选择个数过多，放弃选择
-            if (currentSelectFeatureCount > GlobeStatus.PixelMaxFeaturesSelect)
-            {
-                return;
-            }
-            if (distanceChange < 0)
-            {
-                //int CurrentTrackPointCount = m_TrackPointList.Count;
-
-                //for (int i = CurrentTrackPointCount - 1; i >= 0; i--)
-                //{
-                //    if (m_TrackPointList[i].DistanceToCenter > currentRadius)
-                //    {                        
-                //        m_TrackPointList[i].NewLineFeedback = null;
-                //        m_TrackPointList.RemoveAt(i);                        
-                //    }
-                //}
-
-                //if (m_LastTrackPointCount > m_TrackPointList.Count)
-                //{
-                //    m_LastTrackPointCount = m_TrackPointList.Count;
-                //    m_LastRadius = currentRadius;
-                //}
-            }
-            else
-            {
-                ISelection pSelection = GlobeStatus.Map.FeatureSelection;
-                IEnumFeatureSetup pEnumFeatureSetup = pSelection as IEnumFeatureSetup;
-                IEnumFeature pRnumFeature = pEnumFeatureSetup as IEnumFeature;
-                pRnumFeature.Reset();
-                IFeature pFeature = pRnumFeature.Next();
-                while (pFeature != null)
-                {
-                    string FeatureLayerName = FunctionCommon.GetLayerNameFromFeature(pFeature);
-                    if (!(GlobeStatus.CheckedPolyLines.Contains(FeatureLayerName)))
-                        continue;
-                    
-                    Tuple<IPoint, IPoint> PointPair=FunctionCommon.GetPointPairFromFeature(pFeature,pointCentre,currentRadius,SelectMode);
-                    if (PointPair.Item1 != null)
-                        m_TrackPointList.Add(new TrackPoint(pointCentre, PointPair.Item1));
-                    if (PointPair.Item2 != null)
-                        m_TrackPointList.Add(new TrackPoint(pointCentre, PointPair.Item2));                  
-
-                    pFeature = pRnumFeature.Next();
-                }                
-                int CurrentTrackPointCount=m_TrackPointList.Count;
-                if (m_LastTrackPointCount < CurrentTrackPointCount)
-                {
-                    //for (int i = m_LastTrackPointCount; i < CurrentTrackPointCount; i++)
-                    //{                        
-                    //    if (m_TrackPointList[i].NewLineFeedback == null)
-                    //    {
-                    //        m_TrackPointList[i].NewLineFeedback = new NewLineFeedback();
-                    //        m_TrackPointList[i].NewLineFeedback.Display = GlobeStatus.ActiveView.ScreenDisplay;
-                    //        m_TrackPointList[i].NewLineFeedback.Start(m_PointCentre);
-                    //        m_TrackPointList[i].NewLineFeedback.MoveTo(m_TrackPointList[i].PointMoveTo);
-                    //    }
-                    //}
-                    m_LastRadius = currentRadius;
-                    m_LastTrackPointCount = CurrentTrackPointCount;
-                }
-            }
+            GlobeStatus.Map.SelectByShape(pGeometry, env, false);    
         }
 
         protected override void OnMouseUp(ESRI.ArcGIS.Desktop.AddIns.Tool.MouseEventArgs arg)
@@ -231,50 +159,40 @@ namespace VerticesToCenter
             m_isMouseDown = false;
 
             //鼠标放开时的点
-            IPoint PointMouseUp = GlobeStatus.ActiveView.ScreenDisplay.DisplayTransformation.ToMapPoint(arg.X, arg.Y);
-            if (PointMouseUp == null)
+            //IPoint PointMouseUp = GlobeStatus.ActiveView.ScreenDisplay.DisplayTransformation.ToMapPoint(arg.X, arg.Y);
+            if (m_PointMouseMoveTo == null)
+            {
+                GlobeStatus.ActiveView.Refresh();
                 return;
+            }
 
             //追踪圆停止
-            //（并存取圆弧，用于构造圆形；
-            //实际上也可以利用圆心(鼠标按下)，圆上一点（鼠标放开）来构造圆形）
-            CircleFeedbackWhenMouseUp(PointMouseUp);            
+            CircleFeedbackWhenMouseUp(m_PointMouseMoveTo);            
 
             //追踪多点停止，修改几何
-            TrackPointFeedBackWhenMouseUp(m_PointCentre, PointMouseUp, GlobeStatus.SelectMode);
+            FeatureSelectWhenMouseUp(m_PointCentre, m_PointMouseMoveTo, GlobeStatus.SelectMode);
             
-            //清空追踪list
-            int TrackPointListCount=m_TrackPointList.Count;
-            //for (int i = TrackPointListCount - 1; i > 0; i--)
-            //{
-            //    if (m_TrackPointList[i].NewLineFeedback != null)
-            //        m_TrackPointList[i].NewLineFeedback = null;
-            //}
-            m_TrackPointList.Clear();
             //刷新窗口图形
             //GlobeStatus.ActiveView.PartialRefresh(esriViewDrawPhase.esriViewGeoSelection, null, null);
             GlobeStatus.ActiveView.Refresh();
-        }
-        private ICircularArc CircleFeedbackWhenMouseUp(IPoint pointMouseUp)
-        {
-            ICircularArc pCircularArc = m_CircleFeedback.Stop();
-            m_CircleFeedback = null;
-            return pCircularArc;
-        }
-       
-        private void TrackPointFeedBackWhenMouseUp(IPoint pointCenter,IPoint pointMouseUp,EnumSelectMode selectMode)
-        {
-            //获得圆形
-            IGeometry pGeometry = FunctionCommon.GetCircleGeometry(pointCenter, pointMouseUp);
 
-            //按圆形选择
-            GlobeStatus.Map.SelectByShape(pGeometry, null, false);
-
-            //选择个数过多，放弃选择
-            if (GlobeStatus.Map.SelectionCount > GlobeStatus.PixelMaxFeaturesSelect)
+            m_TrackPointList.Clear();
+        }
+        private void CircleFeedbackWhenMouseUp(IPoint pointMouseUp)
+        {
+            if(m_CircleFeedback!=null)
             {
-                GlobeStatus.Map.ClearSelection();
+                m_CircleFeedback.Stop();
                 m_CircleFeedback = null;
+            }
+        }
+        private void FeatureSelectWhenMouseUp(IPoint pointCenter, IPoint pointMouseUp, EnumSelectMode selectMode)
+        {        
+            //选择个数过多，放弃选择
+            if (GlobeStatus.Map.SelectionCount > GlobeStatus.MaxFeaturesSelect)
+            {
+                //GlobeStatus.Map.ClearSelection();
+                MessageBox.Show("Too many features selected");
                 return;
             }
 
@@ -304,6 +222,7 @@ namespace VerticesToCenter
                         pPointCollection.UpdatePoint(0, pointCenter);
                     if (PointIDPair.Item2 > 0)
                         pPointCollection.UpdatePoint(PointIDPair.Item2, pointCenter);
+                    pFeature.Shape = (IGeometry)pPointCollection;
                     pFeature.Store();
                 }
                 pFeature = pRnumFeature.Next();
